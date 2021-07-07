de3d89d1790e37302939d020a844b7d2efce408f
Kostiantyn <k.vyun1028@gmail.com>
2021-07-08 00:07:26 +0300

    HW7

--- src/main/java/KostiantynViunichenko/HW7/Task2.java
+++ src/main/java/KostiantynViunichenko/HW7/Task2.java
@@ -8,7 +8,25 @@ package KostiantynViunichenko.HW7;
 //        String b = “3344”;
 //        ”Result: 1144
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String s1, s2;
+        System.out.println("Введите первое слово: ");
+        s1 = scanner.nextLine();
+        System.out.println();
+        System.out.println("Введите второе слово: ");
+        s2 = scanner.nextLine();
 
+        String substr1 = s1.substring(0,2);
+        String substr2 = s2.substring(2,4);
+        String result = substr1+substr2;
 
+        System.out.println("String a = "+ s1);
+        System.out.println("String b = "+ s2);
+        System.out.println("Result: "+result);
+    }
 }
