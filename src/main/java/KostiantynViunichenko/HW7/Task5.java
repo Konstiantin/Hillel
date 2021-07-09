3ae0cf29385bd3bcd5dc9f329f23497e33064824
Kostiantyn <k.vyun1028@gmail.com>
2021-07-09 15:55:13 +0300

    HW7

--- src/main/java/KostiantynViunichenko/HW7/Task5.java
+++ src/main/java/KostiantynViunichenko/HW7/Task5.java
@@ -1,7 +1,8 @@
 package KostiantynViunichenko.HW7;
-
-//“It's the possibility of having a dream come true that makes life interesting.”
-//        Необходимо:а) перевести в верхний регистр все буквы в строке. Вывести
+//        Дана строка:
+//       “It's the possibility of having a dream come true that makes life interesting.”
+//        Необходимо:
+//        а) перевести в верхний регистр все буквы в строке. Вывести
 //        результат в консоль
 //        б) перевести в нижний регистр все буквы в строке. Вывести
 //        результат в консоль
@@ -12,5 +13,21 @@ public class Task5 {
 
     public static void main(String[] args) {
 
+        String str = "It's the possibility of having a dream come true that makes life interesting.";
+        System.out.println("\n" + "a) " + str.toLowerCase()+ "\n");
+        System.out.println("b) "+ str.toUpperCase()+"\n");
+
+        String str2 = " ";
+        str2 = str2 + str.substring(0, 1).toUpperCase();
+
+        for (int i = 1; i < str.length(); i++) {
+            if (" ".equals(str.substring(i - 1, i)))
+                str2 = str2 + str.substring(i, i + 1).toUpperCase();
+            else
+                str2 = str2 + str.substring(i, i + 1);
+        }
+        System.out.println("c) " + str2);
     }
 }
+
+
