35879598d9ecec4543bbb3c68431807263664d69
Kostiantyn <k.vyun1028@gmail.com>
2021-07-27 20:09:32 +0300

    HW_11

---
+++ src/main/java/KostiantynViunichenko/HW_11/Candys/Caramel.java
@@ -0,0 +1,13 @@
+package Candys;
+
+
+public class Caramel extends Candies {
+    @Override
+    public String toString() {
+        return "Caramel = ";
+    }
+
+    public Caramel(int weight) {
+        super(weight);
+    }
+}
---
+++ src/main/java/KostiantynViunichenko/HW_11/Candys/Drops.java
@@ -0,0 +1,12 @@
+package Candys;
+
+public class Drops extends Candies {
+    @Override
+    public String toString() {
+        return "Drops = ";
+    }
+
+    public Drops(int weight) {
+        super(weight);
+    }
+}
